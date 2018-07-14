0e43a12732f2ef85edec72b1611b2230d78fd5b0
RobbieHan <robbie_han@outlook.com>
2018-07-14 18:54:03 +0800

    new project

--- sandboxOA/urls.py
+++ sandboxOA/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from users.views import IndexView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
